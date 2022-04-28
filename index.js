0570910996eb5f4dbc07971fcad5d3eb1236cf2e
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-28 22:19:22 +0600

    mongo connected

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5555;
 
 app.get('/', (req, res) => {
-      res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2bong.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('first')
+    // perform actions on the collection object
+    client.close();
+});
