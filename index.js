08254078d58b48cbbb9d41d74c27c73a30590476
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-29 20:06:34 +0600

    get all

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5555;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2bong.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('first')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('inventory-management').collection('products');
+        // get all products
+        app.get('/inventory/all', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+        });
+    }
+    finally {
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
