48caad73421c2e9c0bbe94e1127b1d2c3d426a2d
Anirudhya <anirudhyaduyti@gmail.com>
2022-05-08 23:11:50 +0600

    delete working

--- index.js
+++ index.js
@@ -88,6 +88,18 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if ('OPTIONS' == req.method) {
+        return res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('find links');
 })
